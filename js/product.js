89de536c742af535c622ae6835575226635030ca
yu19941994 <yishian.yu@gmail.com>
2021-06-11 17:19:04 +0800

    upload all files

--- js/product.js
+++ js/product.js
@@ -1,10 +1,20 @@
-const app = Vue.createApp({
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.11/vue.esm-browser.js'
+
+
+let productModal = {}
+let delProductModal = {}
+const app = createApp({
   data() {
     return {
       url: `https://vue3-course-api.hexschool.io`,
       path: `uy_neish`,
       products: [],
-      content: ''
+      content: '',
+      image: '',
+      tempProduct: {
+        // imagesUrl: []
+      },
+      isNew: false
     }
   },
   methods: {
@@ -14,62 +24,73 @@ const app = Vue.createApp({
       axios.defaults.headers.common['Authorization'] = token
     },
     getProduct () {
-      // let vm = this
       axios.get(`${this.url}/api/${this.path}/admin/products`)
         .then(res => {
-          // console.log(res.data.products)
           if (res.data.success) {
             this.products = res.data.products
-            console.log(res.data.products)
-            console.log(this.products)
-            // this.renderProduct(this.product)
-            // this.renderProduct(this.product)
           }
         })
         .catch(err => console.log(err))
     },
-    // renderProduct (product) {
-    //   this.content = ''
-    //   product.forEach(item => {
-    //     this.content += `
-    //       <tr>
-    //         <td>${item.category}</td>
-    //         <td>${item.title}</td>
-    //         <td width="120">
-    //           ${item.origin_price}
-    //         </td>
-    //         <td width="120">
-    //           ${item.price}
-    //         </td>
-    //         <td width="100">
-    //           ${item.is_enabled? `<span class="text-success">啟用</span>`: `<span v-else>未啟用</span>`}
-            
-    //         </td>
-    //         <td width="120">
-    //           <button type="button" class="btn btn-outline-primary btn-sm" data-id="${item.id}" @click="openProductModal">
-    //             編輯
-    //           </button>
-    //           <button type="button" class="btn btn-sm btn-outline-danger move deleteBtn" data-action="remove"
-    //             data-id="${item.id}" @click="deleteProductModal"> 刪除 </button>
-    //         </td>
-    //       </tr>
-    //     `
-    //     this.$refs.productList.innerHTML = this.content
-    //   })
-    //   productCount.innerHTML = this.data.product.length
-    //   // productList.addEventListener('click', app.deleteSingleProduct)
-    // },
-    openProductModal () {
-      $('#productModal').modal('show')
+    openModal (isNew, item) {
+      this.isNew = isNew
+      if(this.isNew){
+        this.tempProduct = {imgUrl: []}
+      }else{
+        this.tempProduct = {...item} 
+      }
+      productModal.show()
     },
-    deleteProductModal () {
-      $('#delProductModal').modal('show')
+    createImages () {
+      this.tempProduct.imagesUrl = ['']
+    },
+    updateProduct () {
+      let updateUrl = `${this.url}/api/${this.path}/admin/product`
+      let method = 'post'
+      if(!this.isNew){
+        method = 'put'
+        updateUrl = `${this.url}/api/${this.path}/admin/product/${this.tempProduct.id}`
+      }
+      this.tempProduct.origin_price = parseInt(this.tempProduct.origin_price)
+      this.tempProduct.price = parseInt(this.tempProduct.price)
+
+      axios[method](updateUrl,{ data: this.tempProduct })
+        .then(res => {
+          console.log(res)
+          if(res.data.success) {
+            this.getProduct()
+            productModal.hide()
+          }
+        })
+        .catch(err => console.log(err))
+    },
+    deleteProductModal (item) {
+      delProductModal.show()
+      this.tempProduct = {...item}
+    },
+    delProduct () {
+      axios.delete(`${this.url}/api/${this.path}/admin/product/${this.tempProduct.id}`)
+        .then(res => {
+          if(res.data.success) {
+            this.getProduct()
+            delProductModal.hide()
+          }
+        })
+        .catch(err => console.log(err))
     }
   },
   created () {
     this.init()
     this.getProduct()
-  } 
+  },
+  mounted () {
+    productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+      keyboard: false
+    }),
+    delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+      keyboard: false
+    })
+  }
 })
 
 app.mount('#app')
